548861875f76fd2ab2fa53813e07619f45f2f66f
Alireza <alireza2000jadidi@gmail.com>
2024-12-02 16:01:43 -0500

    temp updt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import List
+import re
 from collections import Counter
 
 
@@ -21,8 +22,44 @@ class Advent:
             total_distance += val * count_list[val]
         return total_distance
 
+    def analys_report_part1(self, data: List[List[int]]) -> int:
+        tot_safe = 0
+        for row in data:
+            difs = []
+            for i in range(len(row) - 1):
+                difs.append(row[i] - row[i + 1])
+            tot_safe += (
+                1
+                if all(1 <= x <= 3 for x in difs) or all(-3 <= x <= -1 for x in difs)
+                else 0
+            )
+        return tot_safe
 
-if __name__ == "__main__":
+    def is_valid(self, r: int, l: int, decreasing: bool):
+        dif = r - l
+        return (1 <= dif <= 3 and not decreasing) or (-3 <= dif <= -1 and decreasing)
+
+    def is_valid_row(self, row: List[int]) -> bool:
+        l, r, leng = 0, 1, len(row)
+        decreasing = row[r] - row[l] < 0
+        while r != leng - 1 and l != leng:
+            if not self.is_valid(row[r], row[l], decreasing) and not self.is_valid(
+                row[r + 1], row[l], decreasing
+            ):
+                return False
+            l = r
+            r += 1
+        return True
+
+    def analys_report_part2(self, data: List[List[int]]) -> int:
+        tot_safe = 0
+        for row in data:
+            tot_safe += 1 if self.is_valid_row(row) else 0
+
+        return tot_safe
+
+
+def day1():
     advent = Advent()
     list1: List[int] = []
     list2: List[int] = []
@@ -38,3 +75,23 @@ if __name__ == "__main__":
     print(f"the len of l1: {len(list1)}")
     print(f"the len of l2: {len(list2)}")
     print(f"the total distance is {ans}")
+
+
+def day2():
+    advent = Advent()
+    data = []
+
+    with open("input.txt", "r") as input:
+        for line in input:
+            data.append(list(map(int, re.findall(r"-?\d+", line))))
+        print(f"the total data is: {len(data)}")
+        print(
+            f"the safe number of reports in part 1 is: {advent.analys_report_part1(data)}"
+        )
+        print(
+            f"the safe number of reports in part 2 is: {advent.analys_report_part2(data)}"
+        )
+
+
+if __name__ == "__main__":
+    day2()
--- test.py
+++ test.py
@@ -18,6 +18,28 @@ class TestAdvent(unittest.TestCase):
         ans = self.advent.distance_calc_part2(l1, l2)
         self.assertEqual(ans, 31)
 
+    def test_analyse_part1(self):
+        data = [
+            [7, 6, 4, 2, 1],
+            [1, 2, 7, 8, 9],
+            [9, 7, 6, 2, 1],
+            [1, 3, 2, 4, 5],
+            [8, 6, 4, 4, 1],
+            [1, 3, 6, 7, 9],
+        ]
+        self.assertEqual(self.advent.analys_report_part1(data), 2)
+
+    def test_analyse_part2(self):
+        data = [
+            [7, 6, 4, 2, 1],
+            [1, 2, 7, 8, 9],
+            [9, 7, 6, 2, 1],
+            [1, 3, 2, 4, 5],
+            [8, 6, 4, 4, 1],
+            [1, 3, 6, 7, 9],
+        ]
+        self.assertEqual(self.advent.analys_report_part2(data), 4)
+
 
 if __name__ == "__main__":
     unittest.main()
